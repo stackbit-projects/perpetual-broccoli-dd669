5d1c8902238617cb59cb95b1f521ba4bcf72c5ee
Stackbit Code Editor <projects@stackbit.com>
2021-10-30 23:44:02 +0000

    TestimonialsSection.js: changed

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -1,26 +1,26 @@
 const LogoSection = (props) => {
-  const { title, testimoinals } = props;
-  return (
-    <div
-      className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center"
-    >
-      <h1
-        className="text-3xl tracking-tight sm:text-4xl mb-2"
-        data-sb-field-path=".title">
-        {title}
-      </h1>
-      <div
-        className="flex justify-center items-center"
-        data-sb-field-path=".testimonials">
-        {testimonials.map((testimonial, index) => (
-          <div className="p-6" key={index} data-sb-field-path={`.${index}`}>
-            <p>{testimonial.content}</p>
-            <p>-{testimonial.author}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    const { title, testimoinals } = props;
+    return (
+        <div
+            className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center"
+        >
+            <h1
+                className="text-3xl tracking-tight sm:text-4xl mb-2"
+                data-sb-field-path=".title">
+                {title}
+            </h1>
+            <div
+                className="flex justify-center items-center"
+                data-sb-field-path=".testimonials">
+                {testimonials.map((testimonial, index) => (
+                    <div className="p-6" key={index} data-sb-field-path={`.${index}`}>
+                        <p>{testimonial.content}</p>
+                        <p>-{testimonial.author}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default LogoSection;
