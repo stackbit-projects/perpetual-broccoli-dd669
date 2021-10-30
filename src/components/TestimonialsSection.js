1c0d70e92217917c830fd59a5e40195d2607fced
Stackbit Code Editor <projects@stackbit.com>
2021-10-30 23:43:23 +0000

    TestimonialsSection.js: changed

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -3,7 +3,7 @@ const LogoSection = (props) => {
   return (
     <div
       className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center"
-      data-sb-field-path={annotationPrefix}>
+    >
       <h1
         className="text-3xl tracking-tight sm:text-4xl mb-2"
         data-sb-field-path=".title">
