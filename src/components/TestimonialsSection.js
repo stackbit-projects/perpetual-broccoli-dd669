1f224d5f673f1a4edd01134586b3afc1b846f8d2
Stackbit Code Editor <projects@stackbit.com>
2021-10-30 23:40:16 +0000

    TestimonialsSection.js: changed

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -1,8 +1,7 @@
 const TestimonialsSection = (props) => {
   const { content, author } = props;
   return (
-    <div
-      className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center"
+    <div className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center"
       <div
         className="flex justify-center items-center"
         data-sb-field-path=".logos">
